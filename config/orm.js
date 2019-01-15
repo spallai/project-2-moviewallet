80afae27367d5dcc11a9ff7819b26b6cf03da676
Dylanr57 <dylanr574423@gmail.com>
2019-01-15 11:51:26 -0800

    started making orm

--- config/orm.js
+++ config/orm.js
@@ -1,6 +1,35 @@
 var connection = require("../config/connection.js");
 
+var orm = {
 
+    selectAll: function(table, cb) {
+        var queryString = "SELECT * FROM ??";
+
+        connection.query(queryString, table, function(err,result){
+            if (err) throw err;
+            cb(result);
+        });
+    },
+
+    selectOne: function(table, id, cb) {
+        var queryString = "SELECT * FROM ?? WHERE id = ?";
+
+        connection.query(queryString, [table, id], function(err, result) {
+            if (err) throw err;
+            cb(result);
+        });
+    },
+
+    selectSome: function(table, condition, id, cb) {
+        var queryString = "SELECT * FROM ?? WHERE ?? = ?";
+
+        connection.query(queryString, [table, condition, id], function(err, result){
+            if (err) throw err;
+            cb(result);
+        })
+    }
+
+}
 
 
 
