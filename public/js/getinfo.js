1cb1d2a7e2f14d2885345110446c22141f049eee
Dylanr57 <dylanr574423@gmail.com>
2019-01-21 13:08:58 -0800

    made feed work

--- public/js/getinfo.js
+++ public/js/getinfo.js
@@ -5,7 +5,7 @@ $.get("/imdb/random", function(data){
         console.log(data);
         var link = data.Trailer.split("=");
         console.log(link);
-        $("#trailer").attr("src", "https://www.youtube.com/embed/" + link[1] + "?autoplay=1");
+        $("#trailer-box").attr("src", "https://www.youtube.com/embed/" + link[1] + "?autoplay=1");
         $("#title").text(data.Body.Title);
 
         $("#test").on("click", function(event){
