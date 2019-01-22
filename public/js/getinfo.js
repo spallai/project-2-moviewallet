705ec711f10e8cf918792d6393d92c5224a50b46
Isabel <kuri.isabel@gmail.com>
2019-01-22 10:43:07 -0800

    updated frontend

--- public/js/getinfo.js
+++ public/js/getinfo.js
@@ -2,13 +2,11 @@
 $.get("/imdb/random", function(data){
     
     if(data){
-        console.log(data);
         var link = data.Trailer.split("=");
-        console.log(link);
         $("#trailer-box").attr("src", "https://www.youtube.com/embed/" + link[1] + "?autoplay=1");
         $("#title").text(data.Body.Title);
 
-        $("#test").on("click", function(event){
+        $("#no").on("click", function(event){
             $.post("/api/movies", data) 
             .done(function(data){
                 console.log("Adding movie?");
