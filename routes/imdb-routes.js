e46e665bf88cf74550219a9353818a1d16e2ea16
Steven Brown <stevendbrown08@gmail.com>
2019-01-22 15:30:14 -0800

    Wallet displaying correctly; still needs to pick up user id correctly

--- routes/imdb-routes.js
+++ routes/imdb-routes.js
@@ -7,20 +7,14 @@ module.exports = function(app) {
 
     app.get("/imdb/random", function(req, res) {
         movie(function(result) {
-            console.log(result);
             return res.json(result);
         });
     });
 
     app.post("/api/movies", function(req, res) {
         var newMovie = req.body;
-        console.log(newMovie.Status);
         addMovie.create(newMovie.Body.Title, newMovie.Body.imdbID, newMovie.Body.Rated, newMovie.Body.Genre, newMovie.Body.Plot, newMovie.Body.Actors, newMovie.Body.Poster, newMovie.Status, 1, function(){
-            console.log("It worked?");
         });
         res.end();
     });
-
-    
-
 }
